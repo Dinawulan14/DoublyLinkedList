adeeb00a8a94eb5f1737d80063fdcc1c68941b21
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-11-16 16:15:12 +0700

    Add project files.

---
+++ DoublyLinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DoublyLinkedList
+{
+    class Node
+    {
+        /*Node class represent the node of doubly linked list.
+         * * It consists of the information part and liks to
+         * Its succeeding and preceeding nodes
+         * In term of next and previous nodes.*/
+        public int rollNumber;
+        public string name;
+        public Node next;/*points to the succeeding nodr*/
+        public Node prev;/*points to the preceeding node*/
+    }
+}
+
